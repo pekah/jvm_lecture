2fc82fe7b73d1e2630ee47075f7acb75be1f2cac
eli <superyilin2012@gmail.com>
2018-04-14 11:08:01 +0800

    常量池与助记符

--- src/main/java/com/eli/jvm/classloader/Test1.java
+++ src/main/java/com/eli/jvm/classloader/Test1.java
@@ -4,8 +4,15 @@ package com.eli.jvm.classloader;
  *
  * 对于静态字段来说，只有直接定义了该字段的类才会被初始化；
  * 当一个类在初始化时，要求其父类全部都已经初始化完毕
- *
  * 由于首次主动使用类时才会被初始化，因此同时打印str和str2时，MyParent1 static block只会打印一次
+ *
+ *
+ *
+ * -XX:+TraceClassLoading，用于追踪类的加载信息并打印出来
+ * -XX:+<option>，表示开启option选项
+ * -XX:-<option>，表示关闭option选项
+ * -XX:<option>=<value>，表示将option选项的值设置为value
+ *
  */
 public class Test1 {
     public static void main(String[] args) {
